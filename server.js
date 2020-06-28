b56c3d586b948853871233db701d66cb0aeb78f1
Riky Perdana <perdanariky@gmail.com>
2020-06-28 16:31:18 +0700

    simplify

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ dotenv = require('dotenv').config(),
 express = require('express'),
 mongoDB = require('mongodb'),
 {parse, stringify} = JSON,
-responder = (err, stat) => res.send(stringify(stat)),
 withThis = (obj, cb) => cb(obj),
 
 dbCall = (dbname, action) => mongoDB.MongoClient.connect(
@@ -17,34 +16,19 @@ app = express()
 .use(express.static("public"))
 .post('/dbCall', (req, res) => dbCall(
   req.body.dbName, db => withThis(
-    db.collection(req.body.collName),
-    coll => ({
-      get: () =>
-        coll.find(JSON.parse(req.body.filter || '{}'))
+    {
+      coll: db.collection(req.body.collName),
+      responder: (err, stat) => res.send(stringify(stat)),
+    },
+    ({coll, responder}) => ({
+      get: () => coll.find(JSON.parse(req.body.filter || '{}'))
         .toArray((err, array) => res.send(stringify({data: array}))),
-      add: () => coll.insertOne(
-        req.body.doc,
-        (err, stat) => res.send(stringify(stat))
-      ),
-      update: () => coll.updateOne(
-        {_id: req.body.doc._id}, {$set: req.body.doc},
-        (err, stat) => res.send(stringify(stat))
-      ),
-      remove: () => coll.deleteOne(
-        {_id: req.body._id},
-        (err, stat) => res.send(stringify(stat))
-      ),
-      insertMany: () => coll.insertMany(
-        req.body.documents,
-        (err, stat) => res.send(stringify(stat))
-      ),
-      updateMany: () => coll.updateMany(
-        req.body.filter, req.body.update,
-        (err, stat) => res.send(stringify(stat))
-      ),
-      deleteMany: () => coll.deleteMany({},
-        (err, stat) => res.send(stringify(stat))
-      )
+      add: () => coll.insertOne(req.body.doc, responder),
+      update: () => coll.updateOne({_id: req.body.doc._id}, {$set: req.body.doc}, responder),
+      remove: () => coll.deleteOne({_id: req.body._id}, responder),
+      insertMany: () => coll.insertMany(req.body.documents, responder),
+      updateMany: () => coll.updateMany(req.body.filter, req.body.update, responder),
+      deleteMany: () => coll.deleteMany({}, responder)
     })[req.body.method]()
   )
 ))
